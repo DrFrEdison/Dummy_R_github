e4d670f248c33c3b8de6992ea0a2f4354225ceda
MPK <kurtz@mailbox.org>
2022-07-04 11:07:07 +0200

    rmo & rmv update

--- Q-xx-RMV_XXXXX-V01-00_beverage_YYYYY_parameter.R
+++ Q-xx-RMV_XXXXX-V01-00_beverage_YYYYY_parameter.R
@@ -80,8 +80,8 @@ for(i in 1:length(dt$pred)){
        , sub = paste("Bias =", dt$bias[ i ]))
   xaxisdate(dt$trs[[ i ]]$data$datetime)
   abline( h = dt$para$SOLL[ dt$para$i ], col = "darkgreen", lty = 3, lwd = 1.5)
-  abline( h = dt$para$SOLL[ dt$para$i ] + dt$para$eingriff[ dt$para$i ] * c(1, -1), col = "orange", lty = 3, lwd = 1.5)
-  # abline( h = dt$para$SOLL[ dt$para$i ] + dt$para$sperr[ dt$para$i ] * c(1, -1), col = "red", lty = 3, lwd = 1.5)
+  abline( h = unlist( dt$para$eingriff[ dt$para$i ]), col = "orange", lty = 3, lwd = 1.5)
+  # abline( h = unlist(dt$para$sperr[ dt$para$i ]), col = "red", lty = 3, lwd = 1.5)
 }
 
 # Export for damn xlsx ####
